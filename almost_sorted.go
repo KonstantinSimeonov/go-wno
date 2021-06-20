449077282d5815f366db78834a794882ea4e9f8f
Konstantin Simeonov <kon.simeonov@protonmail.com>
2021-06-20 21:22:48 +0300

    Use terms like monotomous sequence and extrema to look smart

--- almost_sorted.go
+++ almost_sorted.go
@@ -32,7 +32,7 @@ func main() {
 		nums[i+1] = n
 	}
 
-	// array extremas indeces
+	// indices of the starts of monotonous sequences
 	xtrs := make([]int, 0)
 	// cheeky
 	decreasing := false
@@ -49,11 +49,16 @@ func main() {
 		}
 	}
 
+	// there are two points in the array which are extremas
+	// -1 10 2 3 4 1 20 1000001 - 10 and 20
 	if len(xtrs) == 4 && xtrs[1]-xtrs[0] == 1 && xtrs[3]-xtrs[2] == 1 {
 		fmt.Printf("yes\nswap %d %d\n", xtrs[0], xtrs[2]+1)
 		return
 	}
 
+	// a decresing monotonous sequence exists
+	// -1 1 2 3 10 9 8 7 20 21 1000001 - 10 and 20
+	// -1 2 1 1000001 - 2 to 1 - use swap
 	if len(xtrs) == 2 && between(nums[xtrs[0]-1], nums[xtrs[1]], nums[xtrs[0]+1]) && between(nums[xtrs[1]-1], nums[xtrs[0]], nums[xtrs[1]+1]) {
 		// no ternary op makes me sad
 		if xtrs[1]-xtrs[0] == 1 {
